5da68d353dba5b1eccd848d22cbeb977f6cdfb3b
adamsJS23 <adamaguebre@yahoo.fr>
2023-08-19 18:03:52 +0200

    working with form

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import "./index.css";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 4, description: "shirt", quantity: 12, packed: false },
-  { id: 2, description: "shoes", quantity: 12, packed: true },
+  { id: 3, description: "shirt", quantity: 12, packed: false },
+  { id: 4, description: "shoes", quantity: 12, packed: true },
 ];
+
+const optionArr = [];
+for (let i = 1; i <= 20; i++) {
+  optionArr.push(i);
+}
 
 export default function App() {
   return (
@@ -24,10 +29,21 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip</h3>
-    </div>
+      <select>
+        {optionArr.map((option) => (
+          <Option number={option} key={option} />
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
+}
+
+function Option(props) {
+  return <option value={props.number}>{props.number}</option>;
 }
 
 function PackingList() {
@@ -35,7 +51,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
